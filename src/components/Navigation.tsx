8b19502a4981a29164dba4c3526f1400d8d3686f
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-07-09 18:53:38 +0200

    initializing prisma

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -10,33 +10,35 @@ const Navigation = () => {
   }, [isDrawerOpen]);
 
   return (
-    <nav className="sticky w-full z-10 top-0 bg-light-astro-gray-100 dark:bg-dark-astro-gray-800 text-light-astro-blue-500 dark:text-dark-astro-blue-500 shadow-md py-2">
+    <nav className="sticky w-full z-10 top-0 bg-space-dark bg-opacity-80 text-white-pure shadow-md py-2">
       <div className="container mx-auto px-4 flex items-center justify-between">
-        <div className="flex items-center  space-x-4">
-          <Link
-            href="/art"
-            className="hover:text-light-astro-purple-500 dark:hover:text-dark-astro-purple-500"
-          >
+        <Link href="/" className="hover:text-space-neon">
+          Home
+        </Link>
+        <div className="flex items-center space-x-4">
+          <Link href="/art" className="hover:text-space-neon">
             Art
           </Link>
-          <Link
-            href="/categories"
-            className="hover:text-light-astro-purple-500 dark:hover:text-dark-astro-purple-500"
-          >
+          <Link href="/categories" className="hover:text-space-neon">
             Categories
           </Link>
-          <Link
-            href="/breeds"
-            className="hover:text-light-astro-purple-500 dark:hover:text-dark-astro-purple-500"
-          >
+          <Link href="/breeds" className="hover:text-space-neon">
             Breeds
           </Link>
-          <Link
-            href="/about"
-            className="hover:text-light-astro-purple-500 dark:hover:text-dark-astro-purple-500"
-          >
+          <Link href="/about" className="hover:text-space-neon">
             About
           </Link>
+          <button
+            onClick={() => {
+              if (isDrawerOpen) {
+                closeDrawer();
+              } else {
+                openDrawer();
+              }
+            }}
+          >
+            Hello
+          </button>
         </div>
       </div>
     </nav>
--- src/components/SideDrawer.tsx
+++ src/components/SideDrawer.tsx
@@ -11,20 +11,20 @@ const Drawer = () => {
     <div className="fixed inset-0 flex z-50">
       <div
         onClick={closeDrawer}
-        className="fixed inset-0 bg-gray-600 opacity-75"
+        className="fixed inset-0 bg-space-dark opacity-75"
       />
-      <div className="relative w-64 m-8 bg-white rounded-lg shadow-lg">
+      <div className="relative w-64 m-8 bg-space-dark rounded-lg shadow-lg text-white-pure">
         <button
           onClick={closeDrawer}
-          className="absolute top-0 right-0 mt-2 mr-2 text-2xl font-bold leading-none text-black hover:text-red-600"
+          className="absolute top-0 right-0 mt-2 mr-2 text-2xl font-bold leading-none text-white-pure hover:text-space-neon"
         >
           &times;
         </button>
         <div className="px-6 py-4">
-          <h2 className="text-2xl font-bold leading-tight text-black">
+          <h2 className="text-2xl font-bold leading-tight">
             Drawer Title
           </h2>
-          <p className="mt-1 text-sm leading-tight text-gray-600">
+          <p className="mt-1 text-sm leading-tight text-space-medium">
             Here is some more information about this product that is only
             revealed once clicked on.
           </p>
@@ -32,7 +32,7 @@ const Drawer = () => {
         <div className="px-6 py-4">
           <button
             onClick={closeDrawer}
-            className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700"
+            className="px-4 py-2 font-bold text-space-dark bg-space-neon rounded-full hover:bg-white-pure"
           >
             Close Drawer
           </button>
